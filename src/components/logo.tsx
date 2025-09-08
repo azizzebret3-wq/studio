fa8ae1156b4e910a0e9132103ba990486c1f892b
Aziz ZEBRET <azizzebret3@gmail.com>
2025-09-08 17:02:44 +0000

    NotFoundError: Failed to execute 'removeChild' on 'Node': The node to be

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ export function Logo() {
   return (
     <Link href="/dashboard" className="flex items-center gap-3" passHref>
       <div className="relative">
-        <div className="w-10 h-10 bg-gradient-to-r from-violet-500 to-purple-600 rounded-xl flex items-center justify-center shadow-lg floating">
+        <div className="w-10 h-10 bg-gradient-to-r from-violet-500 to-purple-600 rounded-xl flex items-center justify-center shadow-lg">
             <span className="text-white font-black text-lg tracking-tighter drop-shadow-lg">GTC</span>
         </div>
       </div>

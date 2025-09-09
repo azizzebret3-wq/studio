da8fe005a83dc9938b79cf9d14249ce38ae170ca
Aziz ZEBRET <azizzebret3@gmail.com>
2025-09-09 13:27:32 +0000

    JE VEUX FAIR UN DÉPLOIEMENT SUR VERCEL POR FAIRE UN TESTE

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  "projectId": "concours-facile",
-  "appId": "1:95785864672:web:15beea9f0ba5accd90af85",
-  "storageBucket": "concours-facile.firebasestorage.app",
-  "apiKey": "AIzaSyCByDfHvT4IgtMd5ACXIOZVZw0lt2wCdmg",
-  "authDomain": "concours-facile.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "95785864672"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "concours-facile",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:95785864672:web:15beea9f0ba5accd90af85",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "concours-facile.firebasestorage.app",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCByDfHvT4IgtMd5ACXIOZVZw0lt2wCdmg",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "concours-facile.firebaseapp.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "95785864672"
 };
 
 // Initialiser Firebase

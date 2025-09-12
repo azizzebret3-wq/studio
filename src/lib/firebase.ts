a1b63751f5a24d8ccf4c558db8d8c9b19f49679f
Aziz ZEBRET <azizzebret3@gmail.com>
2025-09-12 00:23:12 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,15 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// !! IMPORTANT !!
-// COLLEZ L'OBJET DE CONFIGURATION DE VOTRE NOUVEAU PROJET FIREBASE ICI
 const firebaseConfig = {
-  apiKey: "VOTRE_API_KEY",
-  authDomain: "VOTRE_AUTH_DOMAIN",
-  projectId: "VOTRE_PROJECT_ID",
-  storageBucket: "VOTRE_STORAGE_BUCKET",
-  messagingSenderId: "VOTRE_MESSAGING_SENDER_ID",
-  appId: "VOTRE_APP_ID"
+  apiKey: "AIzaSyAvLZFnsqNXABKvRAT7U-rjxsy8zOFWmwA",
+  authDomain: "gtc-com.firebaseapp.com",
+  projectId: "gtc-com",
+  storageBucket: "gtc-com.firebasestorage.app",
+  messagingSenderId: "271987201667",
+  appId: "1:271987201667:web:d9c12109c573fd8990edb6"
 };
 
 

2d44429b30fa19f8dc5c96d91183589cefc7d19e
Aziz ZEBRET <azizzebret3@gmail.com>
2025-09-25 21:16:55 +0000

    ca marche toujour pas vérifie les api

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
-  model: 'googleai/gemini-1.0-pro-001',
+  plugins: [googleAI({ apiVersion: 'v1' })],
+  model: 'gemini-pro',
 });
